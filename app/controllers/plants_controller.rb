ecf5e4219d80cffc3f8c784b337ad5b4b9a3723b
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-08-30 21:48:35 -0600

    feat: completed routes for plants

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -6,4 +6,33 @@ class PlantsController < ApplicationController
     def show
         @plant = Plant.find(params[:id])
     end
+
+    def new
+
+    end
+
+    def create
+        plant = Plant.create(plant_params)
+        rediect_to "/plants"
+    end
+
+    def edit
+        @plant = Plant.find(params[:id])
+    end
+
+    def update
+        plant = Plant.find(params[:id])
+        plant.update(plant_params)
+        redirect_to "/plants"
+    end
+
+    def destroy
+        plant = Plant.find(params[:id])
+        plant.destroy
+        redirect_to "/plants"
+    end
+private
+  def plant_params
+    params.permit(:plant_pet_name)
+  end
 end
